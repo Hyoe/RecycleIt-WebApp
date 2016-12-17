e12e6371d8391b451cf64d727e0c5964cb49d42f
Hyoe <hyoyoe@gmail.com>
2016-12-17 00:40:00 -0800

    adjusted footer for portfolio

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <div class="large-nav">
         <nav class="navbar navbar-inverse navbar-static-bottom navbar-fixed-bottom">
           <div class="container">
-            <span>RecycleIt! was created in 2016 by four CSUMB Computer Science majors.</span>&nbsp&nbsp<span> © 2016 RecycleIt!</span>
+            <span> © 2016 RecycleIt!</span>
           </div>
         </nav>
     </div>
